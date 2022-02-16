cf6463bcb1d52edc324553eceaf96bc7c136929b
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-02-16 18:06:05 +0100

    created client

---
+++ client/src/client.ts
@@ -0,0 +1,8 @@
+import io from "socket.io-client"
+
+const serverUrl = 'http://localhost:3000'
+const socket = io(serverUrl)
+
+socket.on('connect', ()=>{
+    console.log("Connected with server")
+})
