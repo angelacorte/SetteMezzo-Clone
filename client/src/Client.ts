eee53b80f97e45aec6ed5a581e59cfad08eae300
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-02-19 18:21:10 +0100

    added the possibility for client to fire events and a test

--- client/src/Client.ts
+++ client/src/Client.ts
@@ -1,15 +1,49 @@
 import { Socket } from "socket.io-client";
 
+/**
+ * Wrapper for a @Socket.io web socket client.
+ */
 export class Client {
+    /**
+     * A client socket.
+     */
     private socket: Socket;
 
+    /**
+     * 
+     * @param socket The client socket to wrap.
+     */
     constructor(socket: Socket){
         this.socket = socket;
     }
 
-    public registerEvent(eventName: string, callback:Function){
-        this.socket.on(eventName, (arg: any)=>{
-            callback(arg);
+    /**
+     * Register a listener for a specified event.
+     * @param eventName The name of the event.
+     * @param listener The listener to call when the event is received.
+     */
+    public registerEvent(eventName: string, listener: (...args: any[]) => void){
+        this.socket.on(eventName, (...args: any[])=>{
+            listener(args);
         });
+    }
+
+    /**
+     * Register multiple event listeners.
+     * @param eventMap A map of the events and the respective listeners.
+     */
+    public registerEvents(eventMap: Map<string, (...args: any[]) => void>){
+        eventMap.forEach((value: (...args: any[]) => void, key: string)=>{
+            this.registerEvent(key, value);
+        })
+    }
+
+    /**
+     * Emits the event to the Server.
+     * @param eventName The event name.
+     * @param payLoad The optional payload.
+     */
+    public fireEvent(eventName: string, payLoad?: any){
+        this.socket.emit(eventName, payLoad);
     }
 }
--- client/test/src/client.test.ts
+++ client/test/src/client.test.ts
@@ -3,13 +3,47 @@ import { Client } from '../../src/Client'
 
 describe('My Client', ()=>{
 
-    it('registers event callbacks', ()=>{
-        const socket = new SocketMock();
-        const client = new Client(socket);
-        
-        const myCallback = jest.fn();
-        client.registerEvent('test-event', myCallback);
-        socket.socketClient.emit('test-event');
-        expect(myCallback).toBeCalled();
+    let socket: any, client: Client;
+
+    beforeAll(()=>{
+        //initialize a mock socket and client
+        socket = new SocketMock();
+        client = new Client(socket);
     })
+
+    it('registers event listeners', ()=>{
+        //mock some listener callbacks
+        const myFirstCallback = jest.fn();
+        const mySecondCallback = jest.fn();
+        const myThirdCallback = jest.fn();
+
+        //initialize an event map
+        const eventMap = new Map();
+        eventMap.set('test-event-one', myFirstCallback);
+        eventMap.set('test-event-two', myFirstCallback);
+        eventMap.set('test-event-three', mySecondCallback);
+        eventMap.set('test-event-four', myThirdCallback);
+
+        //register the event map
+        client.registerEvents(eventMap);
+
+        //fire the events
+        socket.socketClient.emit('test-event-one');
+        socket.socketClient.emit('test-event-two');
+        socket.socketClient.emit('test-event-three');
+        
+        expect(myFirstCallback).toHaveBeenCalledTimes(2);
+        expect(mySecondCallback).toBeCalled();
+        expect(myThirdCallback).toHaveBeenCalledTimes(0);
+    });
+
+    it('fires events correctly', ()=>{
+        //register an event listener in the server
+        socket.on('hello', (message: string)=>{
+            expect(message).toEqual("world!")
+        });
+
+        //fire the same event the server listens to
+        client.fireEvent('hello', "world!");
+    });
 })
