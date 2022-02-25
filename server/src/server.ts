04bf2931b31f35de6527a096c24fceb1e66d28c2
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-02-25 20:14:56 +0100

    test server pass, database related test not yet (problem on package MongoClient)

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import { Server, Socket } from 'socket.io'
+
+export const PORT = 3000;
+
+const io = new Server(PORT);
+
+console.log("Server running on port: "+PORT);
+
+io.on('connection', (socket: Socket)=>{
+    console.log("Client with id "+socket.id+" connected");
+})
--- server/test/src/server.test.ts
+++ server/test/src/server.test.ts
@@ -1,6 +1,44 @@
 //TODO server-side tests
+import {app} from '../../src/app'
+const request = require('supertest');
+const dbIndex = require('../../src/models/db.index');
+
+const {MongoClient} = require('mongodb');
+
 
 describe('My Server', ()=>{
 
-    it.todo('Accepts client connections')
+    it.todo('Accepts client connections');
+
+    test("test root path", done =>{
+        request(app).get('/').then((response:any)=>{
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+
 })
+
+describe('database related tests', ()=>{
+
+    let connection:any;
+    let db;
+
+    beforeAll( async () =>{
+        connection = await MongoClient.connect(dbIndex.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        db = await connection.db(dbIndex.__DB_NAME__)
+    });
+
+    afterAll( async () => {
+        await connection.close();
+    });
+
+    test("connection to db", done =>{
+        it.todo('Make db requests');
+
+    })
+})
+
