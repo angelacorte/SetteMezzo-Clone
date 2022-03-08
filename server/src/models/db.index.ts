65c99df7ae302dbeeee946bb20b94176d3737337
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-03-08 16:37:10 +0100

    server and db connect successfully

--- server/src/models/db.index.ts
+++ server/src/models/db.index.ts
@@ -1,18 +1,24 @@
-import {LobbyModel} from "./lobby.model";
+//import {LobbyModel} from "./lobby.model";
+const { MongoClient, ServerApiVersion } = require('mongodb');
 import mongoose from "mongoose";
 mongoose.Promise = global.Promise;
 export const __DB_NAME__ = 'setteMezzo';
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const dbURL = 'mongodb+srv://'+process.env["MONGO_USER"]+':'+ process.env["MONGO_PASS"]+'@'+process.env["MONGO_CLUSTER"]+'.kgpdq.mongodb.net/'+process.env["MONGO_DBNAME"]+'?retryWrites=true&w=majority';
+const dbURL = "mongodb+srv://"+process.env["MONGO_USER"]+":"+ process.env["MONGO_PASS"]+"@"+process.env["MONGO_CLUSTER"]+".kgpdq.mongodb.net/"+process.env["MONGO_DBNAME"]+"?retryWrites=true&w=majority";
 
-/**
- * Defines db params
- */
-export const db = {
-    "mongoose": mongoose,
-    "url": dbURL,
-    //lobby: LobbyModel
-};
+const client = new MongoClient(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+export function connectDB(){
+    client.connect((err:any) => {
+        console.log('Mongo has connected succesfully');
+
+    });
+}
+
+export function disconnectDB() {
+    client.close((err:any) => {
+        console.log('Mongo has disconnected succesfully');
+    });
+}
