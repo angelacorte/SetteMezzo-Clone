75184dba1590532c3acf18e7448b0e55f8c426b7
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-03-17 17:02:57 +0100

    added some game manager tests

--- client/src/GameManager.ts
+++ client/src/GameManager.ts
@@ -1,9 +1,73 @@
-import { Client } from './Client'
-import { GameState } from './model/GameState';
+import { GameState } from './model/game-state/GameState';
+import { Player } from './model/Player';
 
 /**
- * Manages the game state.
+ * Manages the game state and client communication.
  */
 export interface GameManager {
-    /** @todo GameManager model and architecture*/
+    /**
+     * @returns all the players in play.
+     */
+    getPlayers(): Map<string, Player>;
+
+    /**
+     * 
+     * @param playerId The player id.
+     * @throws Player not found error.
+     */
+    getPlayer(playerId: string): Player;
+
+    /**
+     * Registers a player in the Game State.
+     * @param player The player to register.
+     * @throws Player not found error.
+     * @throws Not enough money error.
+     */
+    registerPlayer(player: Player): void;
+
+    /**
+     * All the bets made by players in the form of a map.
+     */
+    getBets(): Map<string, number>;
+
+    /**
+     * Registers a bet from a player, decreasing his money amount accordingly.
+     * @param playerId 
+     * @param amount 
+     */
+    registerBet(playerId: string, amount: number): void;
+}
+
+export class GameManagerImpl implements GameManager {
+    private gameState: GameState;
+
+    constructor(gameState: GameState){
+        this.gameState = gameState;
+    }
+
+    getPlayer(playerId: string): Player {
+        let player = this.gameState.getPlayers().get(playerId);
+        if(!player) throw new Error("Player not found");
+        return player;
+    }
+
+    getPlayers(): Map<string, Player> {
+        return this.gameState.getPlayers();
+    }
+
+    registerPlayer(player: Player): void {
+        this.gameState.getPlayers().set(player.getId(), player);
+    }
+
+    getBets(): Map<string, number> {
+        return this.gameState.getBets();
+    }
+
+    registerBet(playerId: string, amount: number): void {
+        let player = this.getPlayer(playerId);
+        player.removeMoney(amount);
+        let bets = this.gameState.getBets();
+        bets.set(playerId, amount);
+    }
+
 }
---
+++ client/test/src/game-manager.test.ts
@@ -0,0 +1,31 @@
+import { GameManager, GameManagerImpl } from "../../src/GameManager"
+import { SetteMezzoGameStateFactory } from "../../src/model/game-state/GameStateFactory";
+import { Player, PlayerImpl } from "../../src/model/Player";
+
+describe('My Game Manager', ()=>{
+    let gameManager: GameManager;
+    let player: Player;
+    const PLAYER_MONEY = 50;
+
+    beforeAll(()=>{
+        let gameState = new SetteMezzoGameStateFactory().createGameState();
+        gameManager = new GameManagerImpl(gameState);
+        player = new PlayerImpl("Player one", PLAYER_MONEY);
+    })
+
+    test('there are no players initially', ()=>{
+        let players = gameManager.getPlayers();
+        expect(players.size).toEqual(0);
+    })
+
+    test('register player', ()=>{
+        gameManager.registerPlayer(player);
+        expect(gameManager.getPlayers().size).toEqual(1);
+    })
+
+    test('register bet', ()=>{
+        gameManager.registerBet(player.getId(), 10);
+        expect(player.getMoney()).toEqual(PLAYER_MONEY - 10);
+        expect(gameManager.getBets().size).toEqual(1);
+    })
+})
