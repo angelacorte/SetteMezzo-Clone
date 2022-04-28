27d31aa5072524de64430373f7a1ea7d915fd7a7
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2022-04-28 21:33:03 +0200

    refactor function name (getRandomCode)

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,7 +1,16 @@
 import crypto from "crypto";
+
 /**
  * @returns random string that will be used as lobby ID
  */
-export function getRandomLobby(){
+export function getRandomCode(){
     return crypto.randomBytes(3).toString('hex');
+}
+
+/**
+ * get a random int from 0 to max
+ * @param max
+ */
+export function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
 }
