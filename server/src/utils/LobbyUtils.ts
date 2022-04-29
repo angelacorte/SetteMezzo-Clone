49279ab82e825193b9c016d45bdabb6112b4d898
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2022-04-29 23:20:48 +0200

    moved LobbyUtilsImpl.ts into LobbyUtils.ts

--- server/src/utils/LobbyUtils.ts
+++ server/src/utils/LobbyUtils.ts
@@ -1,4 +1,8 @@
-import {Lobby} from "../models/lobby/Lobby";
+import {Lobby, LobbyState} from "../models/lobby/Lobby";
+import {SetteMezzoGameStateFactory} from "../../../client/src/model/game-state/GameStateFactory";
+import {GameManager, GameManagerImpl} from "../../../client/src/GameManager";
+import {PlayerImpl} from "../../../client/src/model/Player";
+let lobbies: Lobby[] = [];
 
 /**
  * Lobby instantiate its params based on Client's preferences
@@ -40,4 +44,54 @@ export interface LobbyUtils {
      * Get all public lobbies
      */
     getPublicLobbies(): Lobby[];
+
+    /**
+     * change the status of the lobby
+     * @param lobbyId
+     * @param state
+     */
+    changeState(lobbyId: string, state: LobbyState): void;
+}
+
+export class LobbyUtilsImpl implements LobbyUtils{
+
+    private gameManager: GameManager | undefined;
+
+    public addLobby(lobby: string, id: string, settings: any): void {
+        /*let game = new SetteMezzoGameStateFactory().createGameState();
+        this.gameManager = new GameManagerImpl(game);
+        this.gameManager.registerPlayer(new PlayerImpl(id, settings.initialSbleuri));*/
+        lobbies.push(new Lobby(lobby, id, LobbyState.CREATED, settings.isOpen, settings.maxParticipants, settings.maxRounds, settings.initialSbleuri));
+    }
+
+    public getLobbies(): Lobby[] {
+        return lobbies;
+    }
+
+    public getPublicLobbies(): Lobby[] {
+        return lobbies.filter(l => l.isOpen());
+    }
+
+    public removeLobby(lobby: string): void {
+        lobbies.some((l, i) => {
+            if(l.getId() === lobby){
+                lobbies.splice(i, 1);
+            }
+        })
+    }
+
+    public changeState(lobbyId: string, state: LobbyState){
+        lobbies.some((l, i) => {
+            if(l.getId() === lobbyId){
+                l.setState(state);
+            }
+        })
+    }
+    // lobbySettings(maxParticipants: number, maxRounds: number, initialSbleuri: number, isOpen: boolean): void {
+    //     this._maxParticipants = maxParticipants;
+    //     this._maxRounds = maxRounds;
+    //     this._initialSbleuri = initialSbleuri;
+    //     this._isOpen = isOpen;
+    // } //maybe useless
+
 }
