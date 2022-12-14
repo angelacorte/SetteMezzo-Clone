2ac9038cba6ebf61c558d64298e25fa99245db51
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-12-14 14:34:38 +0100

    refactored deck to be a type instead of a data structure

--- client/src/model/deck/Deck.ts
+++ client/src/model/deck/Deck.ts
@@ -1,53 +1,15 @@
-import { Card, from, Values, Suits, valueMap } from "../card/Card";
+import { Card, from, Values, Suits, nameOf } from "../card/Card";
 
-export interface Deck {
-    cards: Array<Card>
-}
+export type Deck = Array<Card>
 
-export function createDeck(): Deck {
-    const cards = new Array<Card>()
-    //add the 42 briscola cards...
-    for(let suit in Suits){
-        for(let v in Values){
-            let value = valueMap.get(v);
-            if(value){
-                cards.push(from(`${v} of ${suit}`, suit, value))
-            }
-        }
-    }
-    return { cards: cards }
-}
-
-export function emptyDeck(): Deck {
-    return {cards: new Array<Card>()}
-}
-
-export function shuffle(deck: Deck): Deck {
-    return {cards: [...deck.cards]}
-}
-
-export function draw(deck: Deck): Card {
-    let card = deck.cards.pop()
-    if(!card){
-        throw new Error('Empty deck');
-    } else {
-        return card;
-    }
-}
-
-export function removeCard(deck: Deck, card: Card): Deck {
-    const cards = deck.cards.filter(c => c.name != card.name);
-    return {cards: cards}
-}
-
-export function getCardWithName(deck: Deck, name: string) {
-    let found = deck.cards.filter(value =>
-        value.name == name)
+export function getCardWithName(deck: Deck, name: string): Card {
+    let found = deck.filter(value =>
+        nameOf(value) == name)
         .pop();
     if(!found) throw new Error('Card not found')
     return found
 }
 
-export function isEmpty(deck: Deck) {
-    return deck.cards.length == 0
+export function isEmpty(deck: Deck): Boolean {
+    return deck.length == 0
 }
