ea65ba3a5cc10b5b5675a79f0e721628fc9f411a
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-12-16 15:18:22 +0100

    add card testing

--- client/test/src/card.test.ts
+++ client/test/src/card.test.ts
@@ -1,26 +1,19 @@
-import { Card, Values, Suits, from } from '../../src/model/card/Card'
+import { Card, from, nameOf, Suits, Values, pointValueOf } from "../../src/model/card/Card"
 
-describe('My Card', ()=>{
-    let cardOne: Card
+describe('Card tests', ()=>{
+    let myCard : Card
 
     beforeAll(()=>{
-        cardOne = from("King", Suits.COINS, Values.KING);
+        myCard = from(Suits.SWORDS, Values.KING)
     })
 
-    test('getName', ()=>{
-        expect(cardOne.name).toEqual("King");
+    test('Name of', ()=>{
+        const cardName = nameOf(myCard)
+        expect(cardName).toBe("KING of SWORDS")
     })
 
-    test('getValue', ()=>{
-        expect(cardOne.value).toEqual(0.5);
-    })
-
-    test('card precedences', ()=>{
-        let cardTwo = from("Seven", Suits.CLUBS, Values.SEVEN);
-        expect(cardTwo.value).toBeGreaterThan(cardOne.value);
-    })
-
-    test('getSuit', ()=>{
-        expect(cardOne.suit).toEqual(Suits.COINS);
+    test('Point value', ()=> {
+        const cardValue = pointValueOf(myCard)
+        expect(cardValue).toBe(0.5)
     })
 })
