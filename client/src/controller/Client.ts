5036b6d955649a34afc42375cd4daea4ae185750
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-25 11:23:28 +0100

    created a reactive provider for socket events

---
+++ client/src/controller/Client.ts
@@ -0,0 +1,24 @@
+import { io, Socket } from 'socket.io-client'
+import { SERVER_URL } from '../global'
+import { Observable } from 'rxjs'
+
+export class Client {
+    private socket: Socket
+
+    constructor() {
+        this.socket = io(SERVER_URL)
+    }
+
+    public eventObservable(event: string): Observable<any> {
+        return new Observable((subscribe)=>{
+            this.socket.on(event, (data)=>{
+              subscribe.next(data)
+            })
+        })
+    }
+
+    public sendEvent(event: string, ...data: any[]): Client {
+        this.socket.emit(event, data)
+        return this
+    }
+}
