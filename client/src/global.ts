ea326f9d0638e38fadf562187ad03c3dc479f079
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-25 11:21:39 +0100

    created global constants

---
+++ client/src/global.ts
@@ -0,0 +1,8 @@
+const SERVER_URL = 'http://localhost:3000'
+
+//GAME MODES
+const NEW_LOBBY = "Create a new lobby";
+const JOIN_LOBBY =  "Join a specific lobby";
+const RANDOM_LOBBY = "Join a random lobby";
+
+export { SERVER_URL, NEW_LOBBY, JOIN_LOBBY, RANDOM_LOBBY }
