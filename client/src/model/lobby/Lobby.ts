047223ec8a5b1703640f11088c0a3278b57d3d7f
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-26 17:28:37 +0100

    refactor lobby

--- client/src/model/lobby/Lobby.ts
+++ client/src/model/lobby/Lobby.ts
@@ -1,12 +1,12 @@
 interface LobbySettings {
-    readonly name: String
-    readonly maxPlayers: Number
-    readonly maxRounds: Number
-    readonly initialMoney: Number
+    readonly lobbyName: string
+    readonly maxPlayers: number
+    readonly maxRounds: number
+    readonly initialMoney: number
 }
 
-function createLobby(name: String, maxPlayers: Number, maxRounds: Number, initialMoney: Number): LobbySettings {
-    return { name: name, maxPlayers: maxPlayers, maxRounds: maxRounds, initialMoney: initialMoney }
+function createLobby(name: string, maxPlayers: number, maxRounds: number, initialMoney: number): LobbySettings {
+    return { lobbyName: name, maxPlayers: maxPlayers, maxRounds: maxRounds, initialMoney: initialMoney }
 } 
 
 export { LobbySettings, createLobby }
