c4d8c52ff923694f8e59dadd11dd0092987558b8
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-26 20:28:28 +0100

    update test

--- server/test/src/server.test.ts
+++ server/test/src/server.test.ts
@@ -1,27 +1,9 @@
 //TODO server-side tests
 const request = require('supertest');
-const dbIndex = require('../../src/models/db.index');
-
-const {MongoClient} = require('mongodb');
-
 
 describe('My Server', ()=>{
 
     it.todo('Accepts client connections');
-
-})
-
-describe('database related tests', ()=>{
-
-    beforeAll( async () =>{
-        await dbIndex.connectDB();
-    });
-
-    afterAll( async () => {
-        await dbIndex.disconnectDB();
-    });
-
-    it.todo('Make db requests');
 
 })
 
