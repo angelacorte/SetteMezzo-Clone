bee8a81222d760b8b041f8c2576964932ca504c1
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-26 20:21:55 +0100

    add constant max_value

--- client/src/global.ts
+++ client/src/global.ts
@@ -5,4 +5,6 @@ const NEW_LOBBY = "Create a new lobby";
 const JOIN_LOBBY =  "Join a specific lobby";
 const RANDOM_LOBBY = "Join a random lobby";
 
+export const MAX_VALUE: number = 7.5;
+
 export { SERVER_URL, NEW_LOBBY, JOIN_LOBBY, RANDOM_LOBBY }
