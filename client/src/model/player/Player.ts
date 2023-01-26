adc68e97e090d5de7d9e5b1237d9c47fdc470c4d
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-26 11:02:41 +0100

    removed sbleuri

--- client/src/model/player/Player.ts
+++ client/src/model/player/Player.ts
@@ -3,24 +3,21 @@ import { Card } from "../card/Card"
 export interface Player {
     readonly id: string
     readonly name: string
-    readonly moneyLeft: Number
     readonly hand: Hand
-    readonly bets: Bets
 }
 
 export type Hand = Array<Card>
-export type Bets = Array<Number>
 
-export function newPlayer(id: string, name: string, moneyLeft: Number): Player {
-    return createPlayer(id, name, moneyLeft, new Array<Card>(), new Array<Number>())
+export function newPlayer(id: string, name: string): Player {
+    return createPlayer(id, name, new Array<Card>())
 }
 
-export function createPlayer(id: string, name: string, moneyLeft: Number, hand: Hand, bets: Bets) {
-    return { id: id, name: name, moneyLeft: moneyLeft, hand: hand, bets: bets }
+export function createPlayer(id: string, name: string, hand: Hand) {
+    return { id: id, name: name, hand: hand}
 }
 
 export function giveCard(player: Player, card: Card): Player {
     const newHand = player.hand
     newHand.push(card)
-    return createPlayer(player.id, player.name, player.moneyLeft, newHand, player.bets)
+    return createPlayer(player.id, player.name, newHand)
 }
