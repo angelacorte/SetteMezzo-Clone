a77bc065c83db8c75e14142a538388cc09a63eb7
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-30 21:21:25 +0100

    made game state start at lobby creation

--- client/src/controller/GameSetup.ts
+++ client/src/controller/GameSetup.ts
@@ -5,35 +5,30 @@ import { newPlayer } from "../model/player/Player";
 import { client } from "./Client";
 import { lobby } from "./StartMenu";
 
-const connection = client.connection()
-
-const gamestate = connection
+const gamestate = lobby
     .pipe(
-        switchMap(()=>
+        switchMap(({player, lobby$})=>
             of(newSetteMezzoGame())
+                .pipe(
+                    map((gstate)=>({player, lobby$, gstate}))
+                )
         )
     )
 
 const guestJoining = gamestate
-    .pipe(
-        switchMap(gstate =>
-            lobby
-                .pipe(
-                    switchMap(({lobby$}) =>
-                        client.eventObservable('guest-joined')
-                            .pipe(
-                                map(userData => ({gstate, lobby$, userData}))
-                            )
+        .pipe(
+            switchMap(({player, lobby$, gstate})=>
+                client.eventObservable('guest-joined')
+                    .pipe(
+                        map(userData => ({gstate, lobby$, userData}))
                     )
-                )  
+            )
         )
-    )
 
 guestJoining.subscribe(({gstate, lobby$, userData}) => {
     handleGuestJoined(gstate, userData)
     startGame(gstate, lobby$)
 })
-
 
 function handleGuestJoined(gameState: GameState, userData: any) {
     gameState = addPlayer(gameState, newPlayer(userData.userId, userData.userName))
@@ -42,5 +37,6 @@ function handleGuestJoined(gameState: GameState, userData: any) {
 
 function startGame(gameState: GameState, lobbySettings: LobbySettings) {
     if(!(gameState.players.length == lobbySettings.maxPlayers)) return
+    console.log(`L'ultimo giocatore si è unito. La partita può cominciare!`)
     client.sendEvent('start-game', gameState)
 }
