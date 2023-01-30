1dc7eb83d154b0b7e13231bef2176b61d7425eda
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-30 15:13:38 +0100

    modified Card into model that takes from common folder

---
+++ client/src/model/card/CardModel.ts
@@ -0,0 +1,18 @@
+import {Card, VALUE_MAP} from "@common/card/Card"
+
+export function from(suit: string, value: string): Card {
+    return {suit: suit, value: value}
+}
+
+export function nameOf(card: Card): string {
+    return `${card.value} of ${card.suit}`
+}
+
+export function pointValueOf(card: Card): number {
+    let value = VALUE_MAP.get(card.value)
+    if(value){
+        return value
+    } else {
+        throw new Error('Value not valid')
+    }
+}
