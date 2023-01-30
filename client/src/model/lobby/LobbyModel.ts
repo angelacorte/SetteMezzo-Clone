0e5b4424c0a4d08d5768694a888e56a15a123758
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-30 15:13:27 +0100

    modified Lobby into model that takes from common folder

---
+++ client/src/model/lobby/LobbyModel.ts
@@ -0,0 +1,5 @@
+import {LobbySettings} from "@common/lobby/Lobby";
+
+export function createLobby(name: string, maxPlayers: number, maxRounds: number): LobbySettings {
+    return { lobbyName: name, maxPlayers: maxPlayers, maxRounds: maxRounds }
+} 
