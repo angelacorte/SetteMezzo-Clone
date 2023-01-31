950e4a9b5a456a06cde91e1ef23e2c8318151fa7
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-31 13:55:55 +0100

    implemented main game loop

--- client/src/controller/GameLoop.ts
+++ client/src/controller/GameLoop.ts
@@ -1,4 +1,4 @@
-import { switchMap, map } from 'rxjs'
+import { switchMap, map, Observable } from 'rxjs'
 import { GameState } from '../../../common/game-state/GameState'
 import { Player } from '../../../common/player/Player'
 import { MAX_VALUE } from '../global'
@@ -7,17 +7,44 @@ import {client} from './Client'
 import {player} from './StartMenu'
 import * as stio from './stio'
 
-const nextround = player
-    .pipe(
+const nextround: Observable<{
+    player$: Player
+    gstate: GameState
+    currentP: number
+    currentR: number
+    maxR: number
+}> = player.pipe(
         switchMap(player$ =>
             client.eventObservable('round')
                 .pipe(
-                    map(({gstate, currentP, currentR, maxP}) => ({player$, gstate, currentP, currentR, maxP}))
+                    map(({gstate, currentP, currentR, maxR}) => ({player$, gstate, currentP, currentR, maxR}))
                 ) 
         )
     )
 
-nextround.subscribe(console.log)
+
+nextround.subscribe(async ({player$, gstate, currentP, currentR, maxR}) => {
+    if(currentP == gstate.players.length) {
+        currentP = 0
+        currentR += 1
+        if(currentR > maxR){
+            client.sendEvent('end-game', gstate)
+            return
+        }
+    }
+    console.log("== ROUND #", currentR)
+    console.log(`${gstate.players[currentP].name}'s turn`)
+    if ( gstate.players[currentP].id == player$.id) {
+        try {
+            const g = await askCards(gstate, 0)
+            client.sendEvent('next', {gameState: g.gstate, currentPlayer: currentP+1, currentRound: currentR, maxRounds: maxR})
+        } catch(err) {
+            client.sendEvent('next', {gameState: gstate, currentPlayer: currentP+1, currentRound: currentR, maxRounds: maxR})
+            console.log(err)
+        }
+        
+    }
+})
 
 async function askCards(gstate: GameState, totalValue: number): Promise<{gstate: GameState, handValue: number}> {
     const card = gstate.deck.pop()
