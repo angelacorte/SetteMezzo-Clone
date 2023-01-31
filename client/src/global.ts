3fc31f28f34278b88751f88a241829a77769e385
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-01-31 17:13:52 +0100

    add start value for points

--- client/src/global.ts
+++ client/src/global.ts
@@ -1,3 +1,5 @@
+import exp from "constants";
+
 const SERVER_URL = 'http://localhost:3000'
 
 //GAME MODES
@@ -6,5 +8,7 @@ const JOIN_LOBBY =  "Join a specific lobby";
 const RANDOM_LOBBY = "Join a random lobby";
 
 export const MAX_VALUE: number = 7.5;
+
+export const START_VALUE: number = 0;
 
 export { SERVER_URL, NEW_LOBBY, JOIN_LOBBY, RANDOM_LOBBY }
