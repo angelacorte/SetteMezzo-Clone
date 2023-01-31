f8e5466c5666cddab7a071c0445a794a7aada9bf
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-31 18:37:54 +0100

    add function

--- client/src/model/player/PlayerModel.ts
+++ client/src/model/player/PlayerModel.ts
@@ -5,6 +5,10 @@ export function newPlayer(id: string, name: string): Player {
     return createPlayer(id, name, START_VALUE, START_VALUE)
 }
 
-export function createPlayer(id: string, name: string, points: number, wins: number) {
+function createPlayer(id: string, name: string, points: number, wins: number) {
     return { id: id, name: name, points: points, wins: wins}
+}
+
+export function setPoints(player: Player, points: number): Player {
+    return createPlayer(player.id, player.name, points, player.wins)
 }
