f5e2e16738af97e264c71cf1872d7dc2f5027e91
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-01-31 18:38:14 +0100

    now the player remain in the same position

--- client/src/model/game-state/GameStateModel.ts
+++ client/src/model/game-state/GameStateModel.ts
@@ -34,7 +34,12 @@ export function addPlayers(state: GameState, players: Array<Player>): GameState 
 }
 
 export function updatePlayer(state: GameState, oldPlayer: Player, newPlayer: Player): GameState {
-    const newPlayers = state.players.filter(p => p.id != oldPlayer.id)
-    newPlayers.push(newPlayer)
+    const newPlayers = state.players.map(p => {
+        if(p.id == oldPlayer.id) {
+            return newPlayer
+        } else {
+            return p
+        }
+    })
     return createGameState(state.deck, newPlayers)
 }
