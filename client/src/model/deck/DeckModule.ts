a4b1d83946837287f2407fe277543b23251363bd
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-02-15 16:26:30 +0100

    Added more documentation to modules

--- client/src/model/deck/DeckModule.ts
+++ client/src/model/deck/DeckModule.ts
@@ -2,6 +2,10 @@ import {from, nameOf} from "../card/CardModule";
 import {Deck} from "../../../../common/deck/Deck";
 import {Card, Suits, Values} from "../../../../common/card/Card";
 
+/**
+ * Factory for a deck of cards to play SetteMezzo.
+ * @returns A deck of 40 cards.
+ */
 export function createSetteMezzoDeck(): Deck {
     const cards = new Array<Card>()
     for(let suit in Suits){
@@ -12,10 +16,21 @@ export function createSetteMezzoDeck(): Deck {
     return cards
 }
 
+/**
+ * Factory for an empty deck
+ * @returns An empty deck
+ */
 export function emptyDeck(): Deck {
     return new Array<Card>()
 }
 
+/**
+ * Fetches the given card from the deck
+ * @param deck The deck from which to search for the card
+ * @param name The card's name
+ * @returns The card if present
+ * @throws Card not found exception
+ */
 export function getCardWithName(deck: Deck, name: string): Card {
     let found = deck.filter(value =>
         nameOf(value) == name)
@@ -24,10 +39,20 @@ export function getCardWithName(deck: Deck, name: string): Card {
     return found
 }
 
+/**
+ * Checks if the given deck is empty
+ * @param deck 
+ * @returns 
+ */
 export function isEmpty(deck: Deck): Boolean {
     return deck.length == 0
 }
 
+/**
+ * Shuffles the deck, randomizing its elements
+ * @param deck The deck to shuffle
+ * @returns A new randomized deck
+ */
 export function shuffle(deck: Deck): Deck {
     return deck.sort((a, b) => 0.5 - Math.random());
 }
--- common/lobby/Lobby.ts
+++ common/lobby/Lobby.ts
@@ -1,8 +1,15 @@
+/**
+ * Settings that determine how the game will be played.
+ */
 export interface LobbySettings {
     readonly lobbyName: string
     readonly maxPlayers: number
     readonly maxRounds: number
 }
+
+/**
+ * Models the informations necessary to join a Lobby.
+ */
 export interface LobbyJoining {
     readonly lobbyName: string,
     readonly username: string,
