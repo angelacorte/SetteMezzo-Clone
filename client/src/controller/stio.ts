cfa9e3db1d1943ceeafc0f7d8c1ab844aa4ffdbb
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-02-15 16:40:31 +0100

    added doc for command line interface

--- client/src/controller/stio.ts
+++ client/src/controller/stio.ts
@@ -1,5 +1,11 @@
 import inquirer from "inquirer";
 
+/**
+ * Prompts the user with the given message, waiting for user input
+ * @param message The message to prompt to the user
+ * @param questionType The type of the question
+ * @returns The value of the user response
+ */
 async function askWithMessage(message: string, questionType: any) {
     return inquirer
                 .prompt({
@@ -11,14 +17,29 @@ async function askWithMessage(message: string, questionType: any) {
                 .catch((error)=>Promise.reject(error));
 }
 
+/**
+ * Prompts the user with a question, waiting for a string response
+ * @param message The prompt message
+ * @returns The user response
+ */
 async function askQuestion(message: string): Promise<string> {
     return askWithMessage(message, 'input')
 }
 
+/**
+ * Prompts the user with a question, waiting for a numeric response
+ * @param message The prompt message
+ * @returns The user response
+ */
 async function askNumber(message: string): Promise<number> {
     return askWithMessage(message, 'number')
 }
 
+/**
+ * Prompts the user for a choice
+ * @param choices The choices to prompt to the user
+ * @returns The user choice
+ */
 async function askChoice(choices: Array<string>): Promise<string> {
     return inquirer
                 .prompt({
@@ -31,6 +52,11 @@ async function askChoice(choices: Array<string>): Promise<string> {
                 .catch((error)=> Promise.reject(error));
 }
 
+/**
+ * Asks the user for confirmation
+ * @param message The message to prompt
+ * @returns The yes/no answer
+ */
 async function askConfirmation(message: string): Promise<boolean> {
     return askWithMessage(message, 'confirm')
 }
